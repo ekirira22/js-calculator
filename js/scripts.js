5c6ec13c2e62546eda4dc32d7d1466e6a42a76d9
Eric Maranga <ekirira22@gmail.com>
2016-11-23 07:57:41 +0300

    added other functions to the js folder

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,24 @@ var add = function (number1,number2){
 var number1=parseInt(prompt("Enter number one"));
 var number2=parseInt(prompt("Enter number two"));
 alert(add(number1, number2));
+
+var add = function (number1,number2){
+	return number1 - number2;
+};
+var number1=parseInt(prompt("Enter number one"));
+var number2=parseInt(prompt("Enter number two"));
+alert(add(number1, number2));
+
+var add = function (number1,number2){
+	return number1 * number2;
+};
+var number1=parseInt(prompt("Enter number one"));
+var number2=parseInt(prompt("Enter number two"));
+alert(add(number1, number2));
+
+var add = function (number1,number2){
+	return number1 / number2;
+};
+var number1=parseInt(prompt("Enter number one"));
+var number2=parseInt(prompt("Enter number two"));
+alert(add(number1, number2));
